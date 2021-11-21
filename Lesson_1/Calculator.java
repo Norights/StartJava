121045ebffca637b40ec1bb1194dc77f5bda31fa
Norights <norights2@gmail.com>
2021-11-21 19:04:25 +0300

    Homework 1 v.3

--- Lesson_1/Calculator.java
+++ Lesson_1/Calculator.java
@@ -2,23 +2,23 @@ public class Calculator {
     public static void main(String[] args) {
         int a = 10;
         int b = 4;
-        String c = "^";
+        String arifmeticheskoeDeistvie = "^";
         
-        if (c == "+") {
+        if (arifmeticheskoeDeistvie == "+") {
             System.out.println(a + b);
-        } else if (c == "-") { 
+        } else if (arifmeticheskoeDeistvie == "-") {
             System.out.println(a - b); 
-        } else if (c == "*") { 
+        } else if (arifmeticheskoeDeistvie == "*") {
             System.out.println(a * b);
-        } else if (c == "/") { 
+        } else if (arifmeticheskoeDeistvie == "/") {
             System.out.println(a / b); 
-        } else if (c == "^") {
+        } else if (arifmeticheskoeDeistvie == "^") {
             int result = 1;
             for (int i = 1; i <= b; i++) {
-                result = result * a;
-                    System.out.println ("a to the power of b is equal = " + result); 
-                }
-        } else if (c == "%") { 
+                result *= a;
+                    System.out.println ("a to the power of b is equal = " + result);
+        }
+        } else if (arifmeticheskoeDeistvie == "%") { 
             System.out.println(a % b); 
         }
     }
